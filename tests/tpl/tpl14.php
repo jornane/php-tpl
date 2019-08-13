64959ea0a0b25a8979ad572fdd6d0ed4d2e71aff
François Kooman <fkooman@tuxed.net>
2019-08-13 17:15:49 +0200

    source formatting

--- tests/tpl/tpl14.php
+++ tests/tpl/tpl14.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * Copyright (c) 2019 François Kooman <fkooman@tuxed.net>
+ * Copyright (c) 2018 François Kooman <fkooman@tuxed.net>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
